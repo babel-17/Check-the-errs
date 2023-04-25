edae0f321d29c48a58e3986ae63b2d173b064ea7
babel-17 <chicas8414@yahoo.com>
2023-04-25 11:02:23 -0700

    second.

---
+++ main.js
@@ -0,0 +1,16 @@
+	/*----- constants -----*/
+
+
+	/*----- state variables -----*/
+
+
+	/*----- cached elements  -----*/
+    const startBtn = document.getElementById('start')
+
+	/*----- event listeners -----*/
+startBtn.addEventListener('click', startFunction)
+
+	/*----- functions -----*/
+function startFunction() {
+  console.log('start button event listener working')
+}
